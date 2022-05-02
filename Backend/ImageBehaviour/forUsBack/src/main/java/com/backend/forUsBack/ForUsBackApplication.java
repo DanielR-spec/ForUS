bd46e8e637a988ddc597d130c609fe9efa4f9337
Daniel R <daniel_reyes@javeriana.edu.co>
2022-05-02 13:07:07 -0500

    Configuración Api/Spring + packetes IntelliJ

---
+++ Backend/ImageBehaviour/forUsBack/src/main/java/com/backend/forUsBack/ForUsBackApplication.java
@@ -0,0 +1,13 @@
+package com.backend.forUsBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForUsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForUsBackApplication.class, args);
+	}
+
+}
---
+++ Backend/ImageBehaviour/forUsBack/src/test/java/com/backend/forUsBack/ForUsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.forUsBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForUsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
